5127dcfef2081aca6b87b005188b03ab788e5b90
thatimoreira <thatimoreira1@gmail.com>
2023-11-21 07:07:42 -0300

    Changes return value to 1

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,13 +6,13 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 04:07:42 by tsoares-          #+#    #+#             */
-/*   Updated: 2023/11/13 04:08:25 by tsoares-         ###   ########.fr       */
+/*   Updated: 2023/11/21 07:06:59 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isprint(int c)
 {
 	if (c > 31 && c < 127)
-		return (c);
+		return (1);
 	return (0);
 }
