b4913e589944de8c8f150d5115f0d1c22085b871
thatimoreira <thatimoreira.dev@gmail.com>
2023-12-28 11:42:34 -0300

    Refactors pf_putptr function and adjusts main function for consistent output length with printf while testing the function

--- CommonCore/Rank1/Printf/pf_putptr.c
+++ CommonCore/Rank1/Printf/pf_putptr.c
@@ -6,38 +6,23 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 04:42:27 by tsoares-          #+#    #+#             */
-/*   Updated: 2023/12/28 11:34:44 by tsoares-         ###   ########.fr       */
+/*   Updated: 2023/12/28 06:05:41 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	pf_putptr(void *ptr)
+int	pf_putptr(unsigned long long ptr)
 {
-	int		args_amount;
-	unsigned long long	p;
+	int	args_amount;
 
-	p = (unsigned long long)ptr;
 	args_amount = write(1, "0x", 2);
 	if (ptr == 0)
 		args_amount += write(1, "0", 1);
 	else
-		args_amount += pf_puthexadec(p, 'x');
+	{
+		pf_printptr(ptr);
+		args_amount += pf_puthexadec(ptr);
+	}
 	return (args_amount);
 }
-/*
-int	main (void)
-{
-	#include <stdio.h>
-
-	int	num;
-	void	*pt;
-	int	count;
-	int	count2;
-
-	num = 0;
-	pt = &num;
-	count = pf_putptr(pt);
-	ft_printf("\ncount: %d\n", count);
-	return (0);
-}*/
