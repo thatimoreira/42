c552c724cba80cfc691703aa7e7823cad1802ea6
thatimoreira <thatimoreira.dev@gmail.com>
2023-12-28 07:38:31 -0300

    Adds format data type in pf_puthexadec funtion

--- CommonCore/Rank1/Printf/libftprintf.h
+++ CommonCore/Rank1/Printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 12:16:20 by tsoares-          #+#    #+#             */
-/*   Updated: 2023/12/28 07:08:05 by tsoares-         ###   ########.fr       */
+/*   Updated: 2023/12/28 07:25:32 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int		pf_putchar(int c);
 int		pf_putstr(char *str);
 size_t	pf_strlen(const char *str);
 int		pf_putptr(unsigned long long ptr);
-int		pf_puthexadec(unsigned int num, format);
+int		pf_puthexadec(unsigned int num, const char format);
 
 #endif
