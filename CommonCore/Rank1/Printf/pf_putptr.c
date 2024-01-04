47c63157ae2d2d6a8cbf2373db83cfd6be0856a7
thatimoreira <thatimoreira.dev@gmail.com>
2024-01-04 19:13:57 -0300

    Creates p variable to correctly cast the void pointer to unsigned long long

--- CommonCore/Rank1/Printf/pf_putptr.c
+++ CommonCore/Rank1/Printf/pf_putptr.c
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 04:42:27 by tsoares-          #+#    #+#             */
-/*   Updated: 2023/12/28 19:19:41 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/01/04 19:13:05 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int	pf_putptr(void *ptr)
 {
-	int	args_amount;
+	int			args_amount;
+	unsigned long long	p;
 
+	p = (unsigned long long ptr;)
 	args_amount = write(1, "0x", 2);
 	if (ptr == 0)
 		args_amount += write(1, "0", 1);
