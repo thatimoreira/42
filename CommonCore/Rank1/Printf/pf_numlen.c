c1e6da980a72028d1baba51d3158d52dad7758b8
thatimoreira <thatimoreira.dev@gmail.com>
2024-01-04 20:55:10 -0300

    Fixes de divisor number (changes from 10 to 16)

--- CommonCore/Rank1/Printf/pf_numlen.c
+++ CommonCore/Rank1/Printf/pf_numlen.c
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 19:46:33 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/01/04 19:52:02 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/01/04 20:48:53 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	pf_numlen(unsigned long long num)
 	size_t	num_len;
 
 	num_len = 0;
+	if (num == 0)
+		num_len++;
 	while (num)
 	{
 		num /= 16;
