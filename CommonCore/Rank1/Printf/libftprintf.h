3930a69af1a4a4f085a0ce910826ff0f474b8646
thatimoreira <thatimoreira.dev@gmail.com>
2024-01-04 18:38:31 -0300

    Removes pf_putpercent function prototype as when the '%' specifier is passed as an argument to the ft_printf function, the printf immediately call the write function to print the symbol

--- CommonCore/Rank1/Printf/libftprintf.h
+++ CommonCore/Rank1/Printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 12:16:20 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/01/04 17:26:24 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/01/04 18:36:05 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,5 @@ int		pf_putptr(void *ptr);
 int		pf_puthexadec(unsigned int num, const char format);
 int		pf_putnbr(int num);
 int		pf_put_uint(unsigned int num);
-int		pf_putpercent(void);
 
 #endif
