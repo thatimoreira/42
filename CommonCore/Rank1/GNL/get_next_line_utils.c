9c360586a6007a1c8150b559ccbf6993b66554f1
thatimoreira <thatimoreira1@gmail.com>
2024-01-21 07:39:23 -0300

    Implements auxiliaries functions to count a string length (ft_strlen), to copy a string content to another string (ft_strcpy), to duplicate a string (ft_strdup), to concatenate strings (ft_strjoin), to create a substring (ft_substr) and to find the '\n' character (find_n)

---
+++ CommonCore/Rank1/GNL/get_next_line_utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 21:52:08 by tsoares-          #+#    #+#             */
+/*   Updated: 2024/01/21 01:04:37 by tsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*dst;
+
+	dst = (char *)malloc((ft_strlen(str) + 1) * sizeof(char));
+	if (dst == NULL)
+		return (NULL);
+	dst = ft_strcpy(dst, str);
+	return (dst);
+}
+
+char	*ft_strjoin(char const *str1, char const *str2)
+{
+	char	*str;
+	size_t	i;
+	size_t	s_pos;
+
+	if (!str1 || !str2)
+		return (NULL);
+	i = 0;
+	s_pos = 0;
+	str = (char *)malloc(((ft_strlen(str1) + ft_strlen(str2)) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (str1[i])
+		str[i++] = str1[s_pos++];
+	s_pos = 0;
+	while (str2[s_pos])
+		str[i++] = str2[s_pos++];
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_substr(const char *stg, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	end_substr;
+	size_t	stg_len;
+	size_t	i;
+
+	if (!stg)
+		return (NULL);
+	stg_len = ft_strlen(stg);
+	i = 0;
+	if (stg_len <= start)
+		return (ft_strdup(""));
+	if (stg_len < start + len)
+		end_substr = stg_len;
+	else
+		end_substr = start + len;
+	substr = (char *)malloc((end_substr - start + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	while (start < end_substr)
+		substr[i++] = stg[start++];
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*find_n(const char *st, int c)
+{
+	size_t	i;
+	char	*temp_s;
+
+	i = 0;
+	temp_s = (char *)st;
+	while (temp_s[i])
+	{
+		if (temp_s[i] == (char)c)
+			return ((char *)&temp_s);
+		i++;
+	}
+	return (NULL);
+}
