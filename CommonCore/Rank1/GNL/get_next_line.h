97d4ef56119015a3ab9f9871acc3a921c9c84000
thatimoreira <thatimoreira1@gmail.com>
2024-01-27 22:34:33 -0300

    Indentation correction

--- CommonCore/Rank1/GNL/get_next_line.h
+++ CommonCore/Rank1/GNL/get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:19:12 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/01/21 01:15:01 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/01/27 22:33:41 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	GENT_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h> // to use ssize_t
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h> // to use ssize_t
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 21
+#  define BUFFER_SIZE 21
 # endif
 
 size_t		ft_strlen(const char *s);
@@ -29,4 +29,4 @@ char		*ft_substr(const char *stg, unsigned int start, size_t len);
 char		*find_n(const char *st, int c);
 char		*get_next_line(int fd);
 
-# endif
+#endif
